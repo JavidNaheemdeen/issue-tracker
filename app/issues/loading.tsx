b7b6eaafab6f69af2ce03c07a73a8d5a7ef3316a
Javid.Ndn <javidnaheem2001@gmail.com>
2024-04-08 15:18:16 +0530

    Add loading skeletons

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,18 +1,17 @@
+import { Table } from '@radix-ui/themes'
 import React from 'react'
-import { Button, Table } from '@radix-ui/themes';
-import Link from 'next/link';
-import prisma from '@/prisma/client';
-import IssueStatusBadge from '../components/IssueStatusBadge';
-import delay from 'delay';
-import IssueAction from './IssueAction';
+import IssueStatusBadge from '../components/IssueStatusBadge'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import IssueAction from './IssueAction'
 
-const IssuesPage = async () => {
-   const issues = await prisma.issue.findMany();
-   await delay(2000);
+const LoadingIssuePage = () => {
+
+   const issues = [1, 2, 3, 4, 5]
 
    return (
       <div>
-         <IssueAction />
+         < IssueAction />
          <Table.Root variant='surface'>
             <Table.Header>
                <Table.Row>
@@ -23,17 +22,19 @@ const IssuesPage = async () => {
             </Table.Header>
             <Table.Body>
                {issues.map((issue) => (
-                  <Table.Row key={issue.id}>
+                  <Table.Row key={issue}>
                      <Table.Cell>
-                        {issue.title}
+                        <Skeleton />
                         <div className='block md:hidden'>
-                           <IssueStatusBadge status={issue.status} />
+                           <Skeleton />
                         </div>
                      </Table.Cell>
                      <Table.Cell className='hidden md:table-cell'>
-                        <IssueStatusBadge status={issue.status} />
+                        <Skeleton />
                      </Table.Cell>
-                     <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
+                     <Table.Cell className='hidden md:table-cell'>
+                        <Skeleton />
+                     </Table.Cell>
                   </Table.Row>
                ))}
             </Table.Body>
@@ -42,4 +43,4 @@ const IssuesPage = async () => {
    )
 }
 
-export default IssuesPage
+export default LoadingIssuePage
