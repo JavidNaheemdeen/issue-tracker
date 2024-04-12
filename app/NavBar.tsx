f17e53c655c7aa88d3591f71d1e26ea95af3e6d1
Javid.Ndn <javidnaheem2001@gmail.com>
2024-04-13 00:29:26 +0530

    Adding skelton

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -7,6 +7,7 @@ import { AiFillBug } from "react-icons/ai";
 import classnames from 'classnames';
 import { useSession } from 'next-auth/react';
 import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
+import { Skeleton } from '@/app/components';
 
 const NavBar = () => {
 
@@ -32,7 +33,7 @@ const AuthStatus = () => {
 
    const { status, data: session } = useSession();
 
-   if (status === "loading") return null;
+   if (status === "loading") return <Skeleton width="3rem" />;
 
    if (status === "unauthenticated") return <Link className='nav-link' href='/api/auth/signin'>Log in</Link>
 
