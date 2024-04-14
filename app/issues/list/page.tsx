b26de8780632385610ff103fa43c517e69a1b1cd
Javid.Ndn <javidnaheem2001@gmail.com>
2024-04-14 18:48:02 +0530

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -27,6 +27,12 @@ const IssuesPage = async ({ searchParams }: Props) => {
       ? searchParams.status
       : undefined;
 
+   const orderBy = columns
+      .map((column) => column.value)
+      .includes(searchParams.orderBy)
+      ? { [searchParams.orderBy]: 'asc' }
+      : undefined;
+
    const issues = await prisma.issue.findMany({
       where: {
          status
@@ -49,7 +55,7 @@ const IssuesPage = async ({ searchParams }: Props) => {
                         }}>
                            {column.label}
                         </NextLink>
-                        {column.value === searchParams.orderBy && <ArrowUpIcon className='inline'/>}
+                        {column.value === searchParams.orderBy && <ArrowUpIcon className='inline' />}
                      </Table.ColumnHeaderCell>
                   ))}
                </Table.Row>
